0b405e87d6a18321cd4ab6c16f5e2d286ae1bf95
EYRO311 <eruizo1801@alumno.ipn.mx>
2025-05-27 18:19:24 -0600

    proyecto

--- src/components/Proyectos.js
+++ src/components/Proyectos.js
@@ -1,26 +1,56 @@
 import { motion } from 'framer-motion';
 import { fadeInUp } from '../utils/animations';
+import './Proyectos.css';
+import { useTheme } from "../utils/ThemeContext";
 
 function Proyectos() {
+  const { darkMode } = useTheme();
+
   return (
-    <motion.div
-      id="proyectos"
-      className="card-container"
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true, amount: 0.2 }}
-      transition={{ duration: 0.6 }}
-      variants={fadeInUp}
-    >
-      <div className="card">
-        <h3>Proyecto 1</h3>
-        <p>Descripción del proyecto 1</p>
-      </div>
-      <div className="card">
-        <h3>Proyecto 2</h3>
-        <p>Descripción del proyecto 2</p>
-      </div>
-    </motion.div>
+    <section id="proyectos" className={`projects-section ${darkMode ? "dark" : "light"}`}>
+
+      <motion.div
+        className='project-grid'
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.2 }}
+        transition={{ duration: 0.6 }}
+        variants={fadeInUp}
+      >
+        <div className={`project-card ${darkMode ? "dark" : "light"}`}>
+          <iframe
+            src="/path-to-your-pdf.pdf"
+            title="PDF Preview"
+            className="project-preview"
+          />
+          <div className="project-info">
+            <h3>Reporte Técnico</h3>
+            <p>Documento elaborado para presentar análisis y resultados técnicos.</p>
+            <div className="project-skills">
+              <span>LaTeX</span>
+              <span>Python</span>
+            </div>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <div className={`project-card ${darkMode ? "dark" : "light"}`}>
+          <iframe
+            src="https://www.elite-coach.com.mx/"
+            title="Website Preview"
+            className="project-preview"
+          />
+          <div className="project-info">
+            <h3>Landing Page</h3>
+            <p>Participación como frontend y backend mejorando UX.</p>
+            <div className="project-skills">
+              <span>React</span>
+              <span>Node.js</span>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </section>
   );
 }
 
